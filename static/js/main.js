32750d8aabd39aabbbc8c51d758cceceb2efefa6
lndrchan <untitledaep@gmail.com>
2026-01-13 04:00:47 +0000

    Jan 13, 2026, 4:00 AM

--- static/js/main.js
+++ static/js/main.js
@@ -6,12 +6,23 @@
 // 4: Fear
 // 5: Invader
 // 6: Slow power
+// 7: time passes
 
 var phase = 0;
-var activeListItemClass = 'list-group-item-dark';
 
 var phaseList = null;
 var phaseListLength = 0;
+var maxPhaseListHeight = 4;
+var phaseListDict = {
+    0: '<div> <h3 class="phase-list-title">Spirit Phase</h3>    <ul><li>Growth options</li><li>Gain energy</li><li>Choose and pay for cards</li></ul> </div>',
+    1: '<h3 class="phase-list-title">Fast Powers</h3>',
+    2: '<h3 class="phase-list-title">Blighted Island Effect</h3>',
+    3: '<h3 class="phase-list-title">Events</h3>',
+    4: '<h3 class="phase-list-title">Fear Cards</h3> <span class="badge badge-primary badge-pill fear-badge" id="phase-list-fear-badge">2</span>',
+    5: '<div> <h3 class="phase-list-title">Invader Phase</h3>    <ul><li>Ravage</li><li>Build</li><li>Explore</li></ul> </div>',
+    6: '<h3 class="phase-list-title">Slow Powers</h3>',
+    7: '<h3 class="phase-list-title">Time Passes</h3>'
+};
 
 var fearProgress = null;
 var leftBarFearBadge = null;
@@ -20,19 +31,19 @@ var fear = 0;
 var earnedFearCards = 0;
 var fearMax = 8;
 
-// Initialize when DOM is ready
-$( document ).ready(function() {
+var cardDisplay = null;
 
-    phaseList = $('.list-group-item');
+// To store serialised game state for gamesaves persisting between sessions
+var state = [];
+
+// Initialisations
+jQuery(function() {
+
+    phaseList = $('#phase-list');
     fearProgress = $('#fear-progress');
 
-    // Highlight the first phase on page load
-    if (phaseList.length > 0) {
-        setPhase(0);
-        phaseListLength = phaseList.length;
-    }
+    phaseListLength = 8;
 
-    // Add click handler to the button
 
     $('#btn-next-phase').on('click', function() {
         nextStep();
@@ -45,9 +56,11 @@ $( document ).ready(function() {
 
     leftBarFearBadge = $('#left-bar-fear-badge');
     phaseListFearBadge = $('#phase-list-fear-badge');
-    if (leftBarFearBadge && phaseListFearBadge) {
-        updateFearBadge();
-    }
+    updateFearBadge();
+
+    cardDisplay = $('#main-card-display');
+
+    setPhase(0);
 });
 
 
@@ -55,17 +68,24 @@ $( document ).ready(function() {
 // Add click handler to the button
 function nextStep() {
 
+    if (phase === 4 && earnedFearCards > 0) {
+        drawCard('fear');
+        earnedFearCards--;
+        updateFearBadge();
+        return;
+    }
+
     setPhase((phase + 1) % phaseListLength);
 
     if (phase === 4) {
-        if (earnedFearCards > 0) {
+        if (earnedFearCards === 0) {
+            setPhase((phase + 1) % phaseListLength);
+        }
+        else {
             drawCard('fear');
             earnedFearCards--;
             updateFearBadge();
             return;
-        }
-        else if (earnedFearCards === 0) {
-            setPhase((phase + 1) % phaseListLength);
         }
     }
 
@@ -87,8 +107,8 @@ function addFear() {
         fear = 0;
     }
     
-    fearProgress.setAttribute('style', 'width: ' + fear / fearMax * 100 + '%');
-    fearProgress.innerHTML = fear + ' / ' + fearMax;
+    fearProgress.attr('style', 'width: ' + fear / fearMax * 100 + '%');
+    fearProgress.html(fear + ' / ' + fearMax);
 }
 
 function removeFear() {
@@ -108,7 +128,7 @@ function earnFearCard() {
 }
 
 function updateFearBadge() {
-    console.log('Update badge to show ' + earnedFearCards + ' fear cards. ')
+    phaseListFearBadge = $('#phase-list-fear-badge');
     if (earnedFearCards == 0) {
         phaseListFearBadge.hide();
     }
@@ -116,15 +136,13 @@ function updateFearBadge() {
         phaseListFearBadge.show();
     }
 
-    leftBarFearBadge.innerHTML = earnedFearCards;
-    phaseListFearBadge.innerHTML = earnedFearCards;
+    leftBarFearBadge.html(earnedFearCards);
+    phaseListFearBadge.html(earnedFearCards);
 
 }
 
 // Function to draw and display a random card
 function drawCard(type) {
-    // Get the card display div
-    const cardDisplay = document.getElementById('main-card-display');
 
     if (cardDisplay) {
         // Create image element
@@ -146,9 +164,8 @@ function drawCard(type) {
 
         img.className = 'game-card';
 
-        // Clear previous content and add new image
-        cardDisplay.innerHTML = '';
-        cardDisplay.appendChild(img);
+        clearCardDisplay();
+        cardDisplay.append(img);
 
         console.log(`drawCard: ${type}, ${random}.jpg`);
     }
@@ -156,15 +173,39 @@ function drawCard(type) {
 
 // Function to set phase programmatically
 function setPhase(index) {
-    phaseList[phase].classList.remove(activeListItemClass);
+    console.log('Set phase to be ' + index);
+
+    let clearDisplayPhases = [0, 1, 2, 5, 6, 7];
+    if (clearDisplayPhases.includes(index)) {
+        clearCardDisplay();
+    }
+
+    let phaseListHTML = '';
     
     if (index >= 0 && index < phaseListLength)
         phase = index;
     else
         phase = 0;
 
-    phaseList[phase].classList.add(activeListItemClass);
+        for (let i = 0; i < maxPhaseListHeight; i++) {
 
-    const currentPhaseName = phaseList[phase].querySelector('.phase-list-title').textContent;
-    console.log('setPhase: ', currentPhaseName);
+        if (i == 0) {
+            phaseListHTML += '<div class="list-group-item d-flex justify-content-between align-items-center list-group-item-dark">';
+        }
+        else {
+            phaseListHTML += '<div class="list-group-item d-flex justify-content-between align-items-center">';
+        }
+
+        phaseListHTML += phaseListDict[(i + phase) % phaseListLength];
+
+        phaseListHTML += '</div>';
+    }
+
+    phaseList.html(phaseListHTML);
+
+    updateFearBadge();
+}
+
+function clearCardDisplay() {
+    cardDisplay.html('');
 }
