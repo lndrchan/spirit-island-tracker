2c19cfc3b3b1c00440767fd42545771cf5b5044e
lndrchan <untitledaep@gmail.com>
2026-01-15 08:45:09 +0000

    Jan 15, 2026, 8:45 AM

--- static/js/main.js
+++ static/js/main.js
@@ -399,19 +399,19 @@ function generateBadge(terrain) {
             b.html('Jungle');
             break;
         case 'm': 
-            b.attr('background-color', '#111111');
+            b.attr('background-color', '#606060');
             b.html('Mountain');
             break;
         case 's': 
-            b.attr('background-color', '$yellow-200');
+            b.attr('background-color', '#111111');
             b.html('Sand');
             break;
         case 'w': 
-            b.attr('background-color', '$cyan-300');
+            b.attr('background-color', '#111111');
             b.html('Wetland');
             break;
         case 'u': 
-            b.attr('background-color', '$gray-300');
+            b.attr('background-color', '#111111');
             b.html('Unknown');
             break;
         case 'n': 
