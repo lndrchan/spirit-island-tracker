54743649031f387c36a7d3e8528ad887339f296f
lndrchan <untitledaep@gmail.com>
2026-02-01 01:07:20 +0000

    Feb 1, 2026, 1:07 AM

--- main.js
+++ main.js
@@ -701,11 +701,8 @@ function updateUI() {
     $('#next-event-card-btn').attr('disabled','');
     $('#last-fear-card-btn').attr('disabled','');
     $('#next-fear-card-btn').attr('disabled','');
-    if (eventSeqIndex > 0) {
-        $('this-event-card-btn').removeAttr('disabled');
-        console.log($('this-event-card-btn'));
-    } 
-    if (fearSeqIndex > 0) $('this-fear-card-btn').removeAttr('disabled');  
+    if (eventSeqIndex > 0) $('#this-event-card-btn').removeAttr('disabled');
+    if (fearSeqIndex > 0) $('#this-fear-card-btn').removeAttr('disabled');  
 
 }
 
